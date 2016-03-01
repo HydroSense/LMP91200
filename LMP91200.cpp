89e95e68216e27c765e9f0d8542e721bca228cf2
Ward Prescott <wardprescott95@gmail.com>
2016-02-29 23:59:17 -0500

    Real Update

--- LMP91200.cpp
+++ LMP91200.cpp
@@ -3,37 +3,71 @@
 
 
 LMP91200::LMP91200(int cs){
-	_mask = 0000000000000000;
+	_mask = 0x0000;  //Default settings
 	_cs = cs;
 }
 
 void LMP91200::begin(){
+	//Checked and works. -WP
 	SPI.begin();
 	SPI.beginTransaction(SPISettings(4000000, MSBFIRST, SPI_MODE3));
 	pinMode(_cs, OUTPUT);
 }
 
 boolean LMP91200::setPGA(uint16_t PGASet){
-	_mask |= (PGASet << 11); //Set the bit
-	_mask &= ~(PGASet << 11); //Clear the bit
-	Serial.println(_mask, BIN);
+	//Checked and works. -WP
+	
+	//Serial.print("In:  ");
+	//Serial.println(_mask, BIN);
+	uint16_t shiftedMask = (PGASet << 11);
+	_mask &= ~(0b1 << 11); //Clear the bit
+	_mask = shiftedMask | _mask;  //Or the cleared initial mask with new
+	//Serial.print("Out: ");
+	//Serial.println(_mask, BIN);
+	//Serial.println();
 	return sendPacket(_mask);
 }
 
 boolean LMP91200::setVCM(uint16_t VCMSet){
-	return true;
+	//Checked and works. -WP
 
+	uint16_t shiftedMask = (VCMSet << 8);
+	_mask &= ~(0b111 << 8);  //Clear the bits of interest
+	_mask = shiftedMask | _mask;  //Or the cleared initial mask with new
+	return sendPacket(_mask);
 }
 
-boolean LMP91200::setVCOM(uint16_t VCOMSet){
-	return true;
+boolean LMP91200::setVOCM(uint16_t VCOMSet){
+	//Checked and works. -Wp
+	
+	// Serial.print("In:  ");
+	// Serial.println(_mask, BIN);
+	uint16_t shiftedMask = (VCOMSet << 7);
+	_mask &= ~(0b1 << 7); //Clear the bit
+	_mask = shiftedMask | _mask;  //Or the cleared initial mask with new
+	// Serial.print("Out: ");
+	// Serial.println(_mask, BIN);
+	// Serial.println();
+	return sendPacket(_mask);
 }
 
 boolean LMP91200::setDIAG(uint16_t DIAGSet){
-	return true;
+	//Checked and works. -Wp
+	
+	// Serial.print("In:  ");
+	// Serial.println(_mask, BIN);
+	uint16_t shiftedMask = (DIAGSet << 6);
+	_mask &= ~(0b1 << 6); //Clear the bit
+	_mask = shiftedMask | _mask;  //Or the cleared initial mask with new
+	// Serial.print("Out: ");
+	// Serial.println(_mask, BIN);
+	// Serial.println();
+	return sendPacket(_mask);
 }
 
 boolean LMP91200::sendPacket(uint16_t bits){
+	//Checked and works. -WP
+	
 	digitalWrite(_cs, LOW);
 	SPI.transfer16(bits);
 	uint16_t returnedVal = SPI.transfer16(bits);
--- LMP91200.h
+++ LMP91200.h
@@ -3,16 +3,22 @@
 
 #include "Arduino.h"
 
-#define PGA_5 0; //Set the PGA at 5Volts/Volt
-#define PGA_10 1;  //Set the PGA at 10 Volts/Volt
+#define PGA_5 0 //Set the PGA at 5Volts/Volt
+#define PGA_10 1  //Set the PGA at 10 Volts/Volt
 
-//VCM Masking
+#define VCM_78 0b011
+#define VCM_34 0b010
+#define VCM_58 0b001
+#define VCM_12 0b000
+#define VCM_38 0b101
+#define VCM_14 0b110
+#define VCM_18 0b111
 
-#define VOCM_VOCM 
-#define VOCM_GND
+#define VOCM_VOCM 0
+#define VOCM_GND 1
 
-#define DIAG_EN 
-#define DIAG_OFF
+#define DIAG_EN 1
+#define DIAG_OFF 0
 
 class LMP91200
 {
@@ -21,7 +27,7 @@ public:
 	void begin();
 	boolean setPGA(uint16_t PGASet);
 	boolean setVCM(uint16_t VCMSet);
-	boolean setVCOM(uint16_t VCOMSet);
+	boolean setVOCM(uint16_t VCOMSet);
 	boolean setDIAG(uint16_t DIAGSet);
 private:
 	int _cs;
